de13dcc3ee90c0c8e5f6fdc9bf0e04a6aff3a3a5
John Tobón <67936930+devsmart2020@users.noreply.github.com>
2021-04-13 19:40:17 -0500

    EstructuraApi

---
+++ Oesia.Api/Base/InitialStartUp.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Oesia.Services.Mapping;
+using System.Text;
+
+namespace Oesia.Api.Base
+{
+    public class InitialStartUp
+    {
+        private IConfiguration Configuration { get; }
+
+        public InitialStartUp(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // Configurar Servicios
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                  "CorsPolicy",
+                  builder => builder.WithOrigins("http://localhost:4200")
+                  .AllowAnyMethod()
+                  .AllowAnyHeader()
+                  .AllowCredentials());
+            });
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Carvajal WebApi", Version = "v1" });
+            });
+
+
+            //JWT
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Issuer"],
+                    RequireExpirationTime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])) //Configuration["JwtToken:SecretKey"]
+                };
+            });
+
+            //Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("Oesia", new OpenApiInfo
+                {
+                    Title = "Oesia Api RestFull",
+                    Version = "Versión 1",
+                    Description = "Api prueba técnica proceso selección Oesia",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "John Tobón",
+                        Email = "jjhontobon@gmail.com"
+                    }
+                });
+
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "JWT Aauthentication",
+                    Description = "Ingrese JWT Bearer token **_only_**",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+                c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {securityScheme, new string[] { } }
+                });
+            });
+
+            //Contexto de datos
+            //services.AddDbContext<db_carvajalPruebaContext>(options =>
+            //options.UseSqlServer(Configuration.GetConnectionString("carvajal"), b => b.MigrationsAssembly("WebApi.Infrastructure.Data")));
+
+            //AutoMapper
+            services.AddAutoMapper(typeof(MappingProfile));
+
+
+            //services.AddTransient<ILoginRepository, LoginRepository>();
+            //services.AddTransient<ILoginService, LoginService>();
+            //services.AddTransient<IUsuarioRepository, UsuarioRepository>();
+            //services.AddTransient<IUsuarioService, UsuarioService>();
+
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Oesia.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ Oesia.Services/Mapping/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Oesia.Services.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+        }
+    }
+}
